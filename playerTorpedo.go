7173b580c61a10ceaa063cee5e822ef570ab1ce1
aurebrus <aurebrus@gmail.com>
2018-11-23 22:21:06 +0100

    torpedo first

--- playerTorpedo.go
+++ playerTorpedo.go
@@ -10,6 +10,7 @@ const (
 type playerTorpedo struct {
 	texture *sdl.Texture
 	x, y    float64
+	active  bool
 }
 
 func newPlayerTorpedo(renderer *sdl.Renderer) (tor playerTorpedo) {
@@ -18,6 +19,9 @@ func newPlayerTorpedo(renderer *sdl.Renderer) (tor playerTorpedo) {
 }
 
 func (tor *playerTorpedo) draw(renderer sdl.Renderer) {
+	if !tor.active {
+		return
+	}
 	x := tor.x - playerTorpedoSize/2
 	y := tor.y - playerTorpedoSize/2
 
@@ -25,4 +29,22 @@ func (tor *playerTorpedo) draw(renderer sdl.Renderer) {
 		&sdl.Rect{X: 0, Y: 0, W: playerTorpedoSize, H: playerTorpedoSize},
 		&sdl.Rect{X: int32(x), Y: int32(y), W: playerTorpedoSize, H: playerTorpedoSize})
 
+}
+
+var torpedoPool []*playerTorpedo
+
+func initTorpedoPool(renderer *sdl.Renderer) {
+	for i := 0; i < 30; i++ {
+		tor := newPlayerTorpedo(renderer)
+		torpedoPool = append(torpedoPool, &tor)
+	}
+}
+
+func torpedoFromPool() (*playerTorpedo, bool) {
+	for _, tor := range torpedoPool {
+		if !tor.active {
+			return tor, true
+		}
+	}
+	return nil, false
 }
